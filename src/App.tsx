1946ee81a4ef83984c20b02b82afedd59d2bf735
AmarPasalic <apasalic087@gmail.com>
2024-06-24 15:18:14 +0200

    mini nav gotov

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 
 import './App.css'
+import MiniNav from './assets/compnents/miniNav/MiniNav'
 
 function App() {
-  
+  return(
+    <div className='bigWrap'>
+       <MiniNav/>
+       
+       </div>
+   
+  )
 }
 
 export default App
---
+++ src/assets/compnents/miniNav/MiniNav.tsx
@@ -0,0 +1,14 @@
+import "./mininav.css"
+
+const MiniNav = () => {
+  return (
+    <div className="wrapper">
+      <div className="miniNav">
+        <h1>Home  &gt;  Shop &gt;  Mens &gt;  <span>T-Shirts</span> </h1>
+    </div>
+    </div>
+    
+  )
+}
+
+export default MiniNav
