cf6758c0076b83075ffcce8c289091dadcbdabf9
AmarPasalic <apasalic087@gmail.com>
2024-06-26 20:29:18 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,16 @@ import Review from './compnents/rewies/Review'
 import Products from './compnents/products/Products'
 
 function App() {
-  
 
-  return(
+
+  return (
     <div className='bigWrap'>
-       <MiniNav/>
-       <Buy   images={['sources/t-shirt_pics/image0.png','sources/t-shirt_pics/image1.png','sources/t-shirt_pics/image2.png']}/>
-        <Review />
-        <Products/>
-       </div>
-   
+      <MiniNav />
+      <Buy images={['sources/t-shirt_pics/image0.png', 'sources/t-shirt_pics/image1.png', 'sources/t-shirt_pics/image2.png']} />
+      <Review />
+      <Products />
+    </div>
+
   )
 }
 
